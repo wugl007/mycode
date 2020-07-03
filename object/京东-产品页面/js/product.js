33ebcee2f201d638cb5bd015477ae4c4600713c7
wugl007 <wugl007@163.com>
2020-07-03 11:43:38 +0800

    京东产品页面

--- object/京东-产品页面/js/product.js
+++ object/京东-产品页面/js/product.js
@@ -1,16 +1,31 @@
 //定义产品对象
+// function Product(){
+// //     this.name='HM休闲服登山包'
+// //     this.description='棒棒的，棒棒的，登山一流，服务一流，你好，我好，他也好，太棒了，一口气等上珠穆朗玛峰'
+// //     this.normalPrice=144
+// //     this.groupbuyPrice=120
+// //     this.buySum=100;
+// //     this.images=[
+// //         {small:'../images/s11.jpg',big:'../images/s11.jpg'},
+// //         {small:'../images/s12.jpg',big:'../images/s12.jpg'},
+// //         {small:'../images/s13.jpg',big:'../images/s13.jpg'}
+// //     ]
+// // }
+//定义产品对象
 function Product(){
-    this.name='HM休闲服登山包'
-    this.description='棒棒的，棒棒的，登山一流，服务一流，你好，我好，他也好，太棒了，一口气等上珠穆朗玛峰'
-    this.normalPrice=144
-    this.groupbuyPrice=120
-    this.buySum=100;
-    this.images=[
-        {small:'../images/s11.jpg',big:'../images/s11.jpg'},
-        {small:'../images/s12.jpg',big:'../images/s12.jpg'},
-        {small:'../images/s13.jpg',big:'../images/s13.jpg'}
-    ]
+    this.name = 'HM休闲服登山包';
+    this.description = '棒棒的，登山一流，服务一流，一口气登上珠穆朗玛峰';
+    this.normalPrice = '144';
+    this.groupbuyPrice = '120';
+    this.buySum = 100;
+    this.image = [
+        {small:'images/s11.jpg',big:'../images/s11.jpg'},
+        {small:'images/s12.jpg',big:'../images/s12.jpg'},
+        {small:'images/s13.jpg',big:'../images/s13.jpg'}
+    ];
+
 }
+
 Product.prototype={
     /*普通购买*/
     buy:function(){},
