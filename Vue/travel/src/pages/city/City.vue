1cd43cf1dce55952123dddddde383e9a4b43685d
wugl007 <wugl007@163.com>
2020-08-01 09:59:24 +0800

    去哪网项目

--- Vue/travel/src/pages/city/City.vue
+++ Vue/travel/src/pages/city/City.vue
@@ -1,20 +1,59 @@
 <template>
   <div>
     <city-header></city-header>
-    <city-search></city-search>
+    <city-search :cities="cities"></city-search>
+    <city-list :cities="cities"
+               :hot="hotCities "
+               :letter="letter">
+    </city-list>
+    <city-alphabet :cities="cities"
+                   @change="handletterClick">
+    </city-alphabet>
   </div>
 </template>
-
 <script>
+import axios from 'axios'
 import CityHeader from './components/Header'
 import CitySearch from './components/Search'
+import CityList from './components/List'
+import CityAlphabet from './components/Alphabet'
 export default {
   name: 'City',
   components: {
     CityHeader,
-    CitySearch
+    CitySearch,
+    CityList,
+    CityAlphabet
+  },
+  data () {
+    return {
+      cities: {},
+      hotCities: [],
+      letter: ''
+    }
+  },
+  methods: {
+    getCityinfo () {
+      axios.get('/api/city.json')
+        .then(this.handleGetCityInfoSucc)
+    },
+    handleGetCityInfoSucc (res) {
+      res = res.data
+      if (res.ret && res.data) {
+        const data = res.data
+        this.cities = data.cities
+        this.hotCities = data.hotCities
+      }
+    },
+    handletterClick (letter) {
+      this.letter = letter
+    }
+  },
+  mounted () {
+    this.getCityinfo()
   }
 }
+
 </script>
 
 <style  scoped>
--- Vue/travel/src/pages/home/components/Header.vue
+++ Vue/travel/src/pages/home/components/Header.vue
@@ -8,7 +8,7 @@
       输入城市/景点/游玩主题</div>
     <router-link to="/city">
     <div class="header-right">
-      {{city}}
+      {{this.$store.state.city}}
       <span class="iconfont arrow-icon">&#xe64a;</span>
     </div>
     </router-link>
@@ -17,10 +17,7 @@
 
 <script>
 export default {
-  name: 'Header',
-  props: {
-    city: String
-  }
+  name: 'HomeHeader'
 }
 </script>
 
